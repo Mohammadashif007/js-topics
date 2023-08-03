43bca48e25cb37370935d4a5823aedc983cc3663
Mohammad Ashif <mohammadashif510@gmail.com>
2023-08-03 16:06:44 +0600

    js function practice

---
+++ function.js
@@ -0,0 +1,45 @@
+// 1. Print hello world
+
+// const hello = function(){
+//     console.log("hello world");
+// }
+
+// hello();
+
+
+// 2. square of a number
+
+// const squareNum = function(n){
+//     const square = n * n;
+//     return square;
+// }
+
+// const result = squareNum(12);
+// console.log(result);
+
+// function squareNum(n){
+//     const square = n*n;
+//     console.log(square);
+// }
+// squareNum(105);
+
+
+// 3. Celsius to Fahrenheit 
+
+// const celsiusToFahrenheit = function(c){
+//     const fahrenheit = c*(9/5)+32;
+//     return fahrenheit;
+// }
+
+// const result = celsiusToFahrenheit(30);
+// console.log(result);
+
+// 4. Rectangle area find 
+
+// const rectangle = function(w, h){
+//     const area = w * h;
+//     return area;
+// }
+
+// const result = rectangle(105, 10);
+// console.log(result);
